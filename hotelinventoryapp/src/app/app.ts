f91767d224cc93baa8c0679416db202a159ddd7f
DanielManjares <danielaldreenm@gmail.com>
2025-09-20 14:54:14 +0800

    added template syntaxes

--- hotelinventoryapp/src/app/app.ts
+++ hotelinventoryapp/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Rooms } from "./rooms/rooms";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'hinv-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [Rooms],
 })
 export class App {
   protected readonly title = signal('hotelinventoryapp');
---
+++ hotelinventoryapp/src/app/rooms/rooms.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hinv-rooms',
+  //imports: [],
+  templateUrl: './rooms.html',
+  styleUrl: './rooms.css'
+})
+export class Rooms {
+
+  hotelName = "Hilton Hotel";
+  numberOfRooms = 10;
+  hideRooms = false;
+
+  toggle(){
+    this.hideRooms = !this.hideRooms;
+  }
+  
+
+}
